98bb80f58a017cd827c0a84370ac70ceb6866b5d
Smail <ismailmansouri571@gmail.com>
2026-05-11 21:53:51 +0100

    fixes

--- src/main/java/com/osm/conditioning/expedition/controller/ExpeditionController.java
+++ src/main/java/com/osm/conditioning/expedition/controller/ExpeditionController.java
@@ -64,10 +64,7 @@ public class ExpeditionController extends BaseControllerImpl<Expedition, Expedit
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiSingleResponse<>(true, "Expedition created successfully", created));
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<ExpeditionDto> getById(@PathVariable UUID id) {
-        return ResponseEntity.ok(expeditionService.getById(id));
-    }
+
 
 
     @GetMapping("/project/{projectId}")
@@ -79,10 +76,7 @@ public class ExpeditionController extends BaseControllerImpl<Expedition, Expedit
     public ResponseEntity<Map<String, Object>> getProjectTraceability(@PathVariable UUID projectId) {
         return ResponseEntity.ok(expeditionService.getProjectTraceability(projectId));
     }
-    @GetMapping("/{id}")
-    public ResponseEntity<ExpeditionDto> getById(@PathVariable UUID id) {
-        return ResponseEntity.ok(expeditionService.getById(id));
-    }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<ExpeditionDto> getById(@PathVariable UUID id) {
